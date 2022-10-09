114767a6d560d84d98a2188b6dbf3af6af8c3849
Bruna Flor <brn.flor@gmail.com>
2022-10-09 10:30:55 +0100

    wip: input component

---
+++ src/components/UI/input.js
@@ -0,0 +1,12 @@
+import classes from "./input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label>{props.label}</label>
+      <input />
+    </div>
+  );
+};
+
+export default Input;
