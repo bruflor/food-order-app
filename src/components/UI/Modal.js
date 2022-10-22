5899b7a9e047dd1cb25bec2cd4b8dd88f954dc54
Bruna Flor <brn.flor@gmail.com>
2022-10-22 15:12:11 +0100

    cart as a modal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,26 @@
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+
+export const Backdrop = () => {
+  return <div className={classes.backdrop}></div>;
+};
+export const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+export const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
