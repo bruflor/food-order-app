6b84e9387209f223579fd81316d5d1965653e217
Bruna Flor <brn.flor@gmail.com>
2022-10-22 14:41:57 +0100

    getting id on input

--- src/components/UI/input.js
+++ src/components/UI/input.js
@@ -3,8 +3,9 @@ import classes from "./input.module.css";
 const Input = (props) => {
   return (
     <div className={classes.input}>
-      <label>{props.label}</label>
-      <input />
+      <label htmlFor={props.input.id}>{props.label}</label>
+
+      <input {...props.input} />
     </div>
   );
 };
